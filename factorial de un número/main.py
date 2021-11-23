8e3de2e1b6c5879a63c3a63445549a356cffd533
eumermondio <cpachecoarjona@gmail.com>
2021-11-23 18:44:46 +0100

    Factorial con bucle while

--- factorial de un número/main.py
+++ factorial de un número/main.py
@@ -1,4 +1,6 @@
-num = int(input("Dime número a saber factorial: "))
-for i in range(num - 1):
-    num = (i + 1) * num
-print(num)
+num = 1
+while num != 0:
+    num = int(input("Dime número a saber factorial, 0 para salir: "))
+    for i in range(num - 1):
+        num = (i + 1) * num
+    print(num)
